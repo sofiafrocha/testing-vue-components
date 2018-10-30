6c21d2de862fb841081b52478e45b8ff0e0cd5ab
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2018-10-30 17:15:40 +0000

    Add first tests

--- tests/badge.spec.js
+++ tests/badge.spec.js
@@ -1,10 +1,34 @@
 import { mount } from '@vue/test-utils';
-import Badge from './components/Badge';
+import Badge from '../components/Badge';
 
 describe('Badge Component', () => {
 	it('mounts correctly', () => {
-		const component = mount(Badge);
+		const component = mount(Badge, {
+			propsData: {
+				value: 3,
+			},
+		});
+		const html = component.html();
 
-		expect(component.html()).toBeDefined();
+		expect(html).toBeDefined();
+	});
+
+	it('shows the right value', () => {
+		const component = mount(Badge, {
+			propsData: {
+				value: 3,
+			},
+		});
+		expect(component.html()).toContain('3</div>');
+	});
+
+	it('has a special class when "has-new" is passed', () => {
+		const component = mount(Badge, {
+			propsData: {
+				value: 3,
+				hasNew: true,
+			},
+		});
+		expect(component.html()).toContain('class="c-Badge has-new"');
 	});
 });
